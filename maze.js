dd85710d7ecd764e61f720e4e8a0114c8f106a48
abel abadi <abelabadi2@gmail.com>
2016-05-12 14:48:01 -0500

    Initial Game implementation
    attached events

--- maze.js
+++ maze.js
@@ -2,5 +2,14 @@
 @abule
 */
 $(function () {
-	
-})();
+	$('#start').on("click",function () {
+		$('#maze .boundary').removeClass("youlose");
+	});
+	$('.boundary').mouseenter(function () {
+		$('#maze .boundary').addClass("youlose");
+		$('#status').html("<b>You lose!</b>");
+	});
+	$('#end').on("click",function () {
+		$('#status').html("<b>You win!</b>");
+	});
+});
