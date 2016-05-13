bcd7340319006739f1e89c6a64c0597e7f8a09dc
Abel <abelabadi2@gmail.com>
2016-05-12 21:57:21 -0500

    refactor to remove fields from global scopes with module pattern

--- maze.js
+++ maze.js
@@ -1,47 +1,59 @@
 /*
 @abule
 */
-var played = false;
-var won = false;
-var lost = false;
+var maze = (function () {
+	var played = false;
+	var won = false;
+	var lost = false;
+
+	function init() {
+		won = false;
+		status("Click the \"S\" to begin.");
+		$('#start').on("click",start);
+	}
+	function start() {
+		//played
+		if (played === true) {
+			$('#maze .boundary').removeClass("youlose");
+		}
+		//you lose!
+		$('.boundary').mouseenter(function () {
+			if (won == true){ // reset game after winning.
+				location.reload();
+			}else{
+				$('#maze .boundary').addClass("youlose");
+				status("<b>You lose!</b>");
+				played = true;
+			}
+			
+			//$(#end).on("click",init());
+		});
+
+		//you win!
+		$('#end').on("click",function () {
+			won = true;
+			status("<b>You win!</b>");
+			//init();
+		});
+
+	}
+	function status(gameStatus) {
+		$('#status').html(gameStatus);
+	}
+	return{
+		init:init,
+		start:start,
+		status:status
+	};
+})();
+
 $(function () {
-	init();
+	maze.init();
 });
 
-function init() {
-	won = false;
-	status("Click the \"S\" to begin.");
-	$('#start').on("click",start);
-}
 
-function start() {
-	//played
-	if (played === true) {
-		$('#maze .boundary').removeClass("youlose");
-	}
-	//you lose!
-	$('.boundary').mouseenter(function () {
-		if (won == true){ // reset game after winning.
-			location.reload();
-		}else{
-			$('#maze .boundary').addClass("youlose");
-			status("<b>You lose!</b>");
-			played = true;
-		}
-		
-		//$(#end).on("click",init());
-	});
 
-	//you win!
-	$('#end').on("click",function () {
-		won = true;
-		status("<b>You win!</b>");
-		//init();
-	});
 
-}
 
-function status(gameStatus) {
-	$('#status').html(gameStatus);
-}
+
 
